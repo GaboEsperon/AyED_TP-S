ef977475635fc0301266f577aa59333ff172c198
GaboEsperon <GaboEsperon@gmail.com>
2024-10-06 03:17:56 -0300

    creacion de archivos de trabajos 1,2,3,4

---
+++ TP-1/punto_3.py
@@ -0,0 +1,15 @@
+"""
+Una persona desea llevar el control de los gastos realizados al viajar en el subte
+rráneo dentro de un mes. Sabiendo que dicho medio de transporte utiliza un es
+quema de tarifas decrecientes (detalladas en la tabla de abajo) se solicita desarro
+llar una función que reciba como parámetro la cantidad de viajes realizados en un 
+determinado mes y devuelva el total gastado en viajes. Realizar también un pro
+grama para verificar el comportamiento de la función.
+Cantidad de viajes                      Valor del pasaje
+____________________________________________________________________________________
+|1 a 20                           |      Averiguar en Internet el valor actualizado|
+|21 a 30                          |      20% de descuento sobre tarifa máxima      |
+|31 a 40                          |      30% de descuento sobre tarifa máxima      |
+|Más de 40                        |      40% de descuento sobre tarifa máxima      |
+|__________________________________________________________________________________|
+"""
